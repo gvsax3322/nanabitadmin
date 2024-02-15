840011dc27dd009e4451e4031eea98ebef719b8b
codefinger10 <codefinger10@gmail.com>
2024-02-15 16:20:13 +0900

    feat : typescript 환경설정 중입니다.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,40 @@ module.exports = {
     es2021: true,
     node: true,
   },
-
-  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
   overrides: [
     {
-      env: { node: true },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: { sourceType: "script" },
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      plugins: ["@typescript-eslint"],
+      rules: {
+        // Additional rules specific to TypeScript files
+      },
     },
   ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
   },
 };
